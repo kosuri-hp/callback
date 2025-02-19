443c59abcd2ba91cbc9bb099e259c3bfbcfef823
kosuri-hp <kosuribhargavi1836@gmail.com>
2025-02-19 21:00:05 +0530

    Add files via upload

---
+++ anonymous.js
@@ -0,0 +1,27 @@
+function processnumber(num1,num2,callback){
+    return callback(num1,num2)
+}
+var result= processnumber(3,4 (function x,y)){
+    return x + y
+}
+
+console.log(x +y);
+#2
+function greet(callback)
+{
+    return callback("Alice")
+}
+var result =greet(function(name))
+{
+    return "Hello,"+ Name +!+
+}
+console.log(result)
+
+#3
+function calculate(x, y, callback){
+    return callback(x, y);
+}
+var result = calculate(10, 5, function(x, y) {
+    return x - y;
+})
+console.log("Difference: " + result)
